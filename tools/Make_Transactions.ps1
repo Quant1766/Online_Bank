cd104cd92a5aebe3dfbe07f9dbad0e933ef13bc4
Sami Koskivaara <sami.koskivaara@fi.fujitsu.com>
2017-02-14 21:23:17 +0200

    Transaction ps1 tool added

---
+++ tools/Make_Transactions.ps1
@@ -0,0 +1,26 @@
+﻿# with this function you can make transactions to the API endpoint
+
+function Invoke-Transaction {
+    param(
+        [int]$SenderID = 1,
+        [int]$ReceiverID = 2,
+        [float]$Amount = 10.0,
+        [string]$TransactionID,
+        [ValidateSet('authorization', 'presentment')]
+        [string]$TransactionType
+    )
+    $transactionURL = "http://127.0.0.1:5000/api/transactions"
+    $ct = "application/json"
+
+    $body = @{
+        senderID = $SenderID
+        receiverID = $ReceiverID
+        amount = $Amount
+        transactionType = $TransactionType
+        transactionID = $TransactionID
+    } | ConvertTo-Json
+
+    Invoke-RestMethod -Uri $transactionURL -Method Post -Body $body -ContentType $ct
+}
+
+Invoke-Transaction -TransactionType authorization -TransactionID 4321Z -Amount 10.0
